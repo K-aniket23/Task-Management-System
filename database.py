98af15fb514052396725be7966615a67df097f10
K-aniket23 <aniketk@apra.in>
2024-01-16 14:47:04 +0530

    Database-table

---
+++ database.py
@@ -0,0 +1,34 @@
+import sqlite3 
+from manager import Manager
+
+class Database_tables:
+    def __init__(self,db_manager):
+        self.db_manager=db_manager
+
+    def create_table(self,db_manager):
+        # admin table ( id,name, username, password)
+        db_manager.execute_command("""CREATE TABLE IF NOT EXISTS ADMIN(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        username TEXT UNIQUE NOT NULL, 
+        password TEXT NOT NULL);""")
+
+        # employee table (userid, name, username, password, task, task id, task_status)
+
+        db_manager.execute_command("""CREATE TABLE IF NOT EXISTS EMPLOYEE(
+            userid INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+        );""")
+
+        # task table (task id, task_name, task_status, userid)
+        db_manager.execute_command("""CREATE TABLE IF NOT EXISTS TASK(
+            taskid INTEGER PRIMARY KEY AUTOINCREMENT,
+            task_name TEXT NOT NULL
+            task_status TEXT NOT NULL,
+            UserID INTEGER REFERENCES EMPLOYEE(userid)
+        );""")
+
+        print("Tables Created or Already exist")
+
